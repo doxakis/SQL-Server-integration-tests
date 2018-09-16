500721c1ddeb715e7fc687331953209becafcc43
Philip Doxakis <5766482+doxakis@users.noreply.github.com>
2018-09-16 16:55:38 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,8 +1,19 @@
+CREATE DATABASE test
+
+USE test
+
+CREATE TABLE dbo.Persons (IDPerson int IDENTITY(1,1) NOT NULL PRIMARY KEY, Name varchar(20) NOT NULL, City varchar(30) NOT NULL)
+
+INSERT INTO Persons (Name, City) VALUES ('Philip1', 'Montreal')
+INSERT INTO Persons (Name, City) VALUES ('Philip2', 'Montreal')
+
+SELECT * FROM Persons
+
 SELECT TOP(1) Name FROM Persons
 
 PRINT('Here 1')
 
-IF EXISTS(SELECT TOP(1) IDPerson FROM Persons)
-    THROW 50000,'THROW TEST',1
+IF NOT EXISTS(SELECT TOP(1) IDPerson FROM Persons)
+    THROW 50000,'No entries.',1
 
 PRINT('Here 2')
